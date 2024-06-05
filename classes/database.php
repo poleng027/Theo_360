f5630813f200e362d87dd236c13e1ad6556f6986
poleng027 <mariapaulaarcoirezdechavez@gmail.com>
2024-06-05 21:16:11 +0800

    up

--- classes/database.php
+++ classes/database.php
@@ -13,7 +13,7 @@ class database
     }
     function viewServices(){
         $con = $this->opencon();
-        return $stmt = $con->query("Select * from packages") ->fetchAll();
+        return $stmt = $con->query("Select service_id, service_name, service_desc, service_price from packages") ->fetchAll(PDO::FETCH_ASSOC);
     }
 
     function check($username, $password) {
@@ -43,4 +43,52 @@ class database
         ->execute([$firstname, $lastname, $username, $password, $email]);
         return $con->lastInsertId();
         }
-}
+
+        function deletePackage($service_id) {
+            try{
+                $con = $this->opencon();
+                    $con->beginTransaction();
+         
+                    $query = $con->prepare("DELETE FROM packages WHERE service_id = ?");
+                    $query->execute([$service_id]);
+         
+                    $con->commit();
+                    return true;
+         
+            } catch (PDOException $e){
+                $con->rollBack();
+                return false;
+        }
+        }
+    }
+    
+//     function viewdata($user_id){
+//             try{
+//             $con = $this->opencon();
+//             $query = $con->prepare("SELECT user_admin.admin_fname, user_admin.admin_lname, user_admin.admin_id, user_admin.admin_name, user_admin.admin_pass, user_admin.admin_email WHERE user_admin.admin_id = ?");
+//             $query->execute([$user_id]);
+//             return $query->fetch();
+//         }
+// }
+// function UpdateUser($user_id, $fname, $lname, $username, $password, $email) {
+//     try{
+//         $con = $this->opencon();
+//         $query = $con->prepare("UPDATE user_admin SET admin_fname=?, admin_lname=?, admin_name=?, admin_pass=?, admin_email=? WHERE admin_id=?");
+//         return $query->execute([$fname, $lname, $username, $password, $email, $user_id]);
+// }
+// }
+// function delete($user_id) {
+//     try{
+//         $con = $this->opencon();
+//             $con->beginTransaction();
+
+//             $query = $con->prepare("DELETE FROM user_admin WHERE admin_id = ?");
+//             $query->execute([$user_id]);
+
+//             $con->commit();
+//             return true;
+//     } catch (PDOException $e){
+//         $con->rollBack();
+//         return false;
+// }
+// }
--- service.php
+++ service.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 require_once('classes/database.php');
 $con = new database();
-
+ 
 if(isset($_POST['addPackage'])){
     $service_name = $_POST['serviceName'];
     $service_desc = $_POST['serviceDesc'];
     $service_price = $_POST['servicePrice'];
-
+ 
       $result = $con->addPackage($service_name, $service_desc, $service_price);
-
+ 
       if($result) {
         header('location:service.php');
       } else {
@@ -16,10 +16,10 @@ if(isset($_POST['addPackage'])){
       }
     }
 ?>
-
+ 
 <!DOCTYPE html>
 <html lang="en">
-
+ 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -28,7 +28,7 @@ if(isset($_POST['addPackage'])){
     <!-- ======= Styles ====== -->
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
-
+ 
 <body>
      <!-- =============== Navigation ================ -->
      <div class="container">
@@ -42,7 +42,7 @@ if(isset($_POST['addPackage'])){
                         <span class="title">Theo 360</span>
                     </a>
                 </li>
-
+ 
                 <li>
                     <a href="index.php">
                         <span class="icon">
@@ -51,7 +51,7 @@ if(isset($_POST['addPackage'])){
                         <span class="title">Dashboard</span>
                     </a>
                 </li>
-
+ 
                 <li>
                     <a href="reservation.php">
                         <span class="icon">
@@ -60,7 +60,7 @@ if(isset($_POST['addPackage'])){
                         <span class="title">Reservation</span>
                     </a>
                 </li>
-
+ 
                 <li>
                     <a href="service.php">
                         <span class="icon">
@@ -69,7 +69,7 @@ if(isset($_POST['addPackage'])){
                         <span class="title">Services</span>
                     </a>
                 </li>
-
+ 
                 <li>
                     <a href="messages.php">
                         <span class="icon">
@@ -78,7 +78,7 @@ if(isset($_POST['addPackage'])){
                         <span class="title">Messages</span>
                     </a>
                 </li>
-
+ 
                 <li>
                     <a href="password.php">
                         <span class="icon">
@@ -87,8 +87,8 @@ if(isset($_POST['addPackage'])){
                         <span class="title">Password</span>
                     </a>
                 </li>
-
-
+ 
+ 
                 <li>
                     <a href="login.php">
                         <span class="icon">
@@ -105,14 +105,14 @@ if(isset($_POST['addPackage'])){
                         <div class="toggle">
                             <ion-icon name="menu-outline"></ion-icon>
                         </div>
-        
+       
                         <div class="search">
                             <label>
                                 <input type="text" placeholder="Search here">
                                 <ion-icon name="search-outline"></ion-icon>
                             </label>
                         </div>
-        
+       
                         <div class="user">
                             <img src="assets/imgs/customer01.jpg" alt="">
                         </div>
@@ -122,7 +122,7 @@ if(isset($_POST['addPackage'])){
     <h2>Package Services</h2>
     <div class="add-service">
         <h3>Add New Service</h3>
-        
+       
         <form method="POST">
             <input type="text" class="form-control" name="serviceName" placeholder="Service Name" required>
             <input type="text" class="form-control" name="serviceDesc" placeholder="Description" required>
@@ -130,54 +130,63 @@ if(isset($_POST['addPackage'])){
             <button type="submit" name="addPackage">Add Service</button>
         </form>
     </div>
-
+ 
     <div class="service-container">
+    <?php
+    $services = $con->viewServices();
+    // $service = $services['service_desc'];
+    foreach($services as $service) {
+       ?>
         <div class="service">
             <div class="card-container">
-                <?php
-                $services = $con->viewServices();
-                foreach($services as $service) {
-                    ?>
                 <form method="post">
                     <div class="card">
-                        <h4 class="card-title"><?php echo $service['service_name']; ?></h4>
-                        <h5 class="card-text"><?php echo $service['service_desc']; ?></h5>
-                        <p class="card-text"> Price: PHP <?php echo $service['service_price'] ?></p>
+                        <h4 class="card-title"><?php echo $service['service_name'];?></h4>
 
-            <div class="actions-1">
-                <input type="hidden" name="id" value="<?php $service['service_id']; ?>">
-                <button class="edit-btn"><ion-icon name="create-outline"></ion-icon>Edit</button>
-                <input type="hidden" name="id" value="<?php echo $sevice['service_id']; ?>">
-                <button class="delete-btn"><ion-icon name="trash-outline"></ion-icon>Delete</button>
-            </div>
-                
+                         
+                        <h5 class="card-text">
+                            <ul>
+                                <?php $descArray = explode(',', $service['service_desc']);?>
+                                <?php foreach ($descArray as $desc) {?>
+                                <li><?php echo trim($desc);?></li>
+                                 <?php }?>
+                            </ul>
+                            </h5>
+                        <p class="card-text"> Price: PHP <?php echo $service['service_price']?></p>
+ 
+                        <div class="actions-1">
+                            <input type="hidden" name="id" value="<?php $service['service_id'];?>">
+                            <button class="edit-btn"><ion-icon name="create-outline"></ion-icon>Edit</button>
+                            <input type="hidden" name="id" value="<?php echo $sevice['service_id'];?>">
+                            <button class="delete-btn"><ion-icon name="trash-outline"></ion-icon>Delete</button>
+                        </div>
                     </div>
                 </form>
             </div>
         </div>
         <?php
-            }
-            ?>
-    </div>
-    
-    
-    </div>
-    
+    }
+   ?>
 </div>
-
-        
-
+   
+   
+    </div>
+   
+</div>
+ 
+       
+ 
         </div>
     </div>
-
+ 
     <!-- =========== Scripts =========  -->
     <script src="assets/js/main.js"></script>
-
-
-
+ 
+ 
+ 
     <!-- ====== ionicons ======= -->
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
-
+ 
 </html>
